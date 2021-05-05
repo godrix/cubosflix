a4df81f76e4a8eb2622896b480fc32ce2f01fd8b
Carlos Godri <cggodri@gmail.com>
2021-05-05 20:16:56 -0300

    create scafold

---
+++ lib/view/cubosflix.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Cubosflix extends StatefulWidget {
+  @override
+  _CubosflixState createState() => _CubosflixState();
+}
+
+class _CubosflixState extends State<Cubosflix> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cubosflix'),
+      ),
+    );
+  }
+}
